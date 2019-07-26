bf0d436bfdd02f42297117ca3c604878660ee292
csaba.adam <csaba.adam@shiwaforce.com>
2019-07-26 10:57:53 +0200

    4

--- app/src/main/java/com/acsabi/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/acsabi/swoosh/WelcomeActivity.kt
@@ -1,12 +1,32 @@
 package com.acsabi.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
+
+        /*
+        Intent: lehet implicit és explicit, valamilyen esemény
+        lehet click-re kamera, link valahova pl browsert nyitni,
+
+        explicit: amikor megmondjuk h mit használjon az adnroid
+        itt pl: LeagueActivity::class.java
+
+        implicit: android dönti el, vagy ajánlja fel (pl link nyitás, melyik böngészővel?)
+
+        saját appban explicit Intent-eket használunk
+
+         */
     }
 }
