d90e8052936aa12922efbe04053e761d69adaea9
csaba.adam <csaba.adam@shiwaforce.com>
2019-07-26 14:07:54 +0200

    7

--- app/src/main/java/com/acsabi/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/acsabi/swoosh/Controller/SkillActivity.kt
@@ -1,12 +1,18 @@
 package com.acsabi.swoosh.Controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.acsabi.swoosh.Utilities.EXTRA_LEAGUE
 import com.acsabi.swoosh.R
+import com.acsabi.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
     var league = ""
+    var skill = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +22,27 @@ class SkillActivity : BaseActivity() {
          */
         league = intent.getStringExtra(EXTRA_LEAGUE)
         println(league)
+    }
+
+    fun onBallerClick(view: View) {
+        beginnerSkillBtn.isChecked = false
+        skill = "baller"
+    }
+
+    fun onBeginnerClick(view: View) {
+        ballerSkillBtn.isChecked = false
+        skill = "beginner"
+    }
+
+    fun onSkillFinishClick(view: View){
+        if (skill != "") {
+            val finishActivity = Intent(this, FinishActivity::class.java)
+            finishActivity.putExtra(EXTRA_LEAGUE, league)
+            finishActivity.putExtra(EXTRA_SKILL, skill)
+            startActivity(finishActivity)
+        } else {
+            Toast.makeText(this, "Please select a skill level", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/acsabi/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/acsabi/swoosh/Utilities/Constants.kt
@@ -1,3 +1,4 @@
 package com.acsabi.swoosh.Utilities
 
 const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
